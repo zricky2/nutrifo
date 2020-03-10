cfe412af72c27a232981c6328318b3c2e9e3dbc4
Ricky Zheng <rickyzheng2@yahoo.com>
2020-03-09 20:10:35 -0400

    commit

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+var node = document.createElement("LI");                 // Create a <li> node
+node.className = "nav-item active";
+    
+//var textnode = document.createTextNode("Login"); 
+var att = document.createElement("a")
+att.textContent = "Login";
+att.href = "login.html";
+att.className = "nav-link";                          
+node.appendChild(att);// Append the text to <li>
+document.getElementById("navul").appendChild(node);     // Append <li> to <ul> with
+
 function openQRCamera(node) {
     var reader = new FileReader();
+    
     reader.onload = function() {
       node.value = "";
       qrcode.callback = function(res) {
@@ -12,8 +24,15 @@ function openQRCamera(node) {
       qrcode.decode(reader.result);
     };
     reader.readAsDataURL(node.files[0]);
+    enableBtn()
+
   };
 
   function showQRIntro() {
     return confirm("Use your camera to take a picture of a QR code.");
+  };
+
+
+  function enableBtn() {
+    document.getElementById("go").disabled = false;
   };
